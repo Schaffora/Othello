8d72b0049dc9573b8f599ba1d087a197cffc9586
Schaffora <schaffora@gmail.com>
2017-01-20 08:53:12 +0100

    all

---
+++ Othello/Othello/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello
+{
+    
+    class Game
+    {
+        public string turn;
+        public int whitePlayerTime;
+        public int blackPlayerTime;
+        public Game()
+        {
+            turn = "white";
+            whitePlayerTime = 0;
+            blackPlayerTime = 0;
+        }
+       
+    }
+}
